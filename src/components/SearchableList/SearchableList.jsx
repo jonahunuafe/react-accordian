1bcc6475f06c397c96d648d18f03253476f5060d
Jonah Unuafe <fejisfejiro@gmail.com>
2024-02-19 15:55:47 +0100

    adding search to the app

--- src/components/SearchableList/SearchableList.jsx
+++ src/components/SearchableList/SearchableList.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
+
 export default function SearchableList({ items }) {
+    const [searchItem, setSearchItem] = useState("");
+
+    const searchResults = items.filter((item) => 
+        JSON.stringify(item).toLowerCase().includes(searchItem.toLowerCase())
+    );
+
+    function handleChange(event) {
+        setSearchItem(event.target.value);
+    }
+
     return (
         <div className="searchable-list">
-            <input type="search" placeholder="Search" />
-
+            <input type="search" placeholder="Search" onChange={handleChange} />
             <ul>
-                {items.map((item, index) => (
+                {searchResults.map((item, index) => (
                     <li key={index}>{item.toString()}</li>
                 ))}
             </ul>
